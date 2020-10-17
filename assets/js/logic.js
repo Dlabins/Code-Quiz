c80476fcaac2b562ab66726e5c235aff5b43e5c4
Dlabins <DLABINS1895@GMAIL.COM>
2020-10-17 17:27:25 -0400

    folder changes

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -96,7 +96,7 @@ function saveHighscore() {
     
     highscores.push(newScore);
     window.localStorage.setItem("highscores", JSON.stringify(highscores));
-    window.location.href = "/assets/highscores.html";
+    window.location.href = "./assets/highscores.html";
   }
 }
 
